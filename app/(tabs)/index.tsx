91a01b1417523d7e3e6d02fd3cb8e6d08f5563a8
Maxim Toyberman <maxim.toyberman@helloheart.com>
2024-12-19 11:03:05 +0200

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,58 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Pressable } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { BleError, BleManager, Characteristic, Device } from 'react-native-ble-plx';
+import { useCallback, useState } from 'react';
+
+let bleManager = new BleManager();
+
+
+const HOMEDICS_DEVICE_NAME = 'Homedics';
+const HOMEDICS_BROADCAST_NAME = 'HHBPA-100BT';
+const HOMEDICS_COMMUNICATION_SERVICE = '0000fff0-0000-1000-8000-00805f9b34fb';
+
+const HOMEDICS_WRITE_CHARACTERISTIC = '0000fff5-0000-1000-8000-00805f9b34fb';
+const HOMEDICS_READ_CHARACTERISTIC = '0000fff4-0000-1000-8000-00805f9b34fb';
 
 export default function HomeScreen() {
+
+  const [device, setDevice] = useState<Device | null>(null); 
+
+  const scanForMonitors = useCallback(async () => {
+
+    const onDeviceDiscovered = async (error: BleError, device: Device) => {  
+      if (error) {
+        // Handle error
+        console.log('Scan error: ', error);
+        return;
+      }
+
+      bleManager.stopDeviceScan();
+
+      setDevice(device);
+    };
+    
+    await bleManager.startDeviceScan([HOMEDICS_COMMUNICATION_SERVICE], null, onDeviceDiscovered);
+  }, []);
+
+  const connectToDevice = useCallback(async () => {
+    if (device) {
+      await device.connect();
+      await device.discoverAllServicesAndCharacteristics();
+      const charachteristics =  await device.characteristicsForService(HOMEDICS_COMMUNICATION_SERVICE);
+
+      const writeCharacteristic = charachteristics?.find((c: Characteristic) => c.uuid === HOMEDICS_WRITE_CHARACTERISTIC);
+      const readCharacteristic = charachteristics?.find((c: Characteristic) => c.uuid === HOMEDICS_READ_CHARACTERISTIC);
+
+
+      // writeCharacteristic.writeWithResponse(base64js.fromByteArray(value))
+
+    }
+   }, [device]);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -20,36 +67,12 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="subtitle">Step 1: Pair monitor</ThemedText>
+        <Pressable style={{justifyContent: 'center', height: 50}} onPress={() => alert('Pressed!')}>
+          <ThemedText type="link">Press to Pair monitor</ThemedText>
+        </Pressable>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+
     </ParallaxScrollView>
   );
 }
